507961e318646995a0c55e54ba9ee4832c75c543
Gaffclant <Gaffclant@gmail.com>
2021-02-13 20:13:19 -0600

    rice

--- dwm-6.2/config.def.h
+++ dwm-6.2/config.def.h
@@ -12,8 +12,10 @@ static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Angel Rhapsody:size=14:anitalias=true:autohint=true" };
-static const char dmenufont[]       = "Angel Rhapsody:size=14:antialias=true:autohint=truev";
+
+static const char *fonts[]          = { "FontAwesome:size=12","Fantasque Sans Mono Nerd Font:size=10:antialias=true:autohint=true", };
+static const char dmenufont[]       = "Fira Sans:size=10:antialias=true:autohint=true";
+
 static const char col_gray1[]       = "#0f0a2b";
 static const char col_gray2[]       = "#261315";
 static const char col_gray3[]       = "#bbbbbb";
@@ -26,7 +28,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -45,9 +47,9 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+	{ "",      tile },    /* first entry is default */
+	{ "",      NULL },    /* no layout function means floating behavior */
+	{ "",      monocle },
 };
 
 /* key definitions */
